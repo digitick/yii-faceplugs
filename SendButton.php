46ac8fb7cf3e051c98a212b5b5dadbc57be11ce1
ianare <ianare@gmail.com>
2012-06-01 00:09:38 +0200

    add new plugins: SendButton, SubscribeButton

---
+++ SendButton.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Wrappers for facebook plugins.
+ * @copyright © Digitick <www.digitick.net> 2011
+ * @license GNU Lesser General Public License v3.0
+ * @author Ianaré Sévi
+ */
+
+require_once 'EFaceplugsBase.php';
+
+/**
+ * The Send Button allows users to easily send content to their friends.
+ * 
+ * People will have the option to send your URL in a message to their Facebook friends,
+ * to the group wall of one of their Facebook groups, and as an email to any email address.
+ * While the Like Button allows users to share content with all of their friends, the
+ * Send Button allows them to send a private message to just a few friends.
+ * 
+ * @see http://developers.facebook.com/docs/reference/plugins/send
+ */
+class SendButton extends EFaceplugsBase
+{
+	/**
+	 * @var string The URL to send.
+	 */
+	public $href;
+	/**
+	 * @var string The font to display in the button. Options: 'arial',
+	 * 'lucida grande', 'segoe ui', 'tahoma', 'trebuchet ms', 'verdana'
+	 */
+	public $font;
+	/**
+	 * @var string The color scheme for the plugin. Options: 'light', 'dark'
+	 */
+	public $colorscheme;
+	/**
+	 * @var string A label for tracking referrals; must be less than 50
+	 * characters and can contain alphanumeric characters and some punctuation
+	 * (currently +/=-.:_).
+	 */
+	public $ref;
+
+	public function run()
+	{
+		parent::run();
+		$this->printTag('send');
+	}
+}
---
+++ SubscribeButton.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Wrappers for facebook plugins.
+ * @copyright © Digitick <www.digitick.net> 2011
+ * @license GNU Lesser General Public License v3.0
+ * @author Ianaré Sévi
+ */
+
+require_once 'EFaceplugsBase.php';
+
+/**
+ * The Subscribe button lets a user subscribe to your public updates on Facebook.
+ * 
+ * @see http://developers.facebook.com/docs/reference/plugins/subscribe
+ */
+class SubscribeButton extends EFaceplugsBase
+{
+	/**
+	 * @var string The profile URL of the user to subscribe to. This must be a facebook.com profile URL.
+	 */
+	public $href;
+	/**
+	 * @var string There are three options:
+	 *
+	 * 'standard' - displays social text to the right of the button and friends' profile photos below.
+	 * Minimum width: 225 pixels. Default width: 450 pixels. Height: 35 pixels (without photos) or 80 pixels (with photos).
+	 *
+	 * 'button_count' - displays the total number of subscribers to the right of the button.
+	 * Minimum width: 90 pixels. Default width: 90 pixels. Height: 20 pixels.
+	 *
+	 * box_count - displays the total number of subscribers above the button.
+	 * Minimum width: 55 pixels. Default width: 55 pixels. Height: 65 pixels.
+	 */
+	public $layout;
+	/**
+	 * @var boolean Display profile photos in the plugin (standard layout only).
+	 */
+	public $show_faces;
+	/**
+	 * @var integer The width of the plugin in pixels.
+	 */
+	public $width;
+	/**
+	 * @var string The font to display in the button. Options: 'arial',
+	 * 'lucida grande', 'segoe ui', 'tahoma', 'trebuchet ms', 'verdana'
+	 */
+	public $font;
+	/**
+	 * @var string The color scheme for the plugin. Options: 'light', 'dark'
+	 */
+	public $colorscheme;
+
+	public function run()
+	{
+		parent::run();
+		$this->printTag('subscribe');
+	}
+}
