a802539d19276d1a73c421dee7259736ab924af6
Evan Johnson <ejohnson@splashlabsocial.com>
2016-02-19 14:23:43 -0700

    Adding missing PresistentDataHandler

---
+++ src/YiiFacebookSessionPersistentDataHandler.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * YiiFacebookSessionPersistentDataHandler class file.
+ *
+ * @author Evan Johnson <thaddeusmt - A T - gmail.com>
+ * @author Ianaré Sévi (original author) www.digitick.net
+ * @link https://github.com/splashlab/yii-facebook-opengraph
+ * @copyright &copy; Digitick <www.digitick.net> 2011
+ * @copyright Copyright &copy; 2015 SplashLab Social  http://splashlabsocial.com
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License v3.0
+ *
+ */
+
+namespace YiiFacebook;
+use Yii;
+use Facebook;
+use Facebook\PersistentData\PersistentDataInterface;
+use Facebook\Exceptions\FacebookSDKException;
+
+class YiiFacebookSessionPersistentDataHandler implements PersistentDataInterface
+{
+    /**
+     * @var string Prefix to use for session variables.
+     */
+    protected $sessionPrefix = 'FBRLH_';
+
+    /**
+     * Init the Yii session handler.
+     *
+     * @param boolean $enableSessionCheck
+     *
+     * @throws FacebookSDKException
+     */
+    public function __construct($enableSessionCheck = true)
+    {
+        if ($enableSessionCheck && !Yii::app()->session) {
+            throw new FacebookSDKException(
+                'Yii Sessions are not active.',
+                720
+            );
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function get($key)
+    {
+        if (isset(Yii::app()->session[$this->sessionPrefix . $key])) {
+            return Yii::app()->session[$this->sessionPrefix . $key];
+        }
+
+        return null;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function set($key, $value)
+    {
+        Yii::app()->session[$this->sessionPrefix . $key] = $value;
+    }
+}
